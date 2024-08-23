74e61eb2eddc39eafc3b7387633a77170924bc95
signoriana <119551197+signoriana@users.noreply.github.com>
2024-08-23 15:38:26 -0300

    perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,152 @@
+export const perguntas = [
+    {
+        enunciado: "1. Quando você enfrenta um novo desafio, como você tende a reagir?",
+        alternativas: [
+            {
+                texto: "a) Encaro com entusiasmo e sou imediatamente proativo.",
+                afirmacao: [
+                    "Você pode ser alguém que gosta de novidades e se sente energizado com a oportunidade de experimentar coisas novas. Isso pode indicar que você é extrovertido e proativo, preferindo um ambiente dinâmico e desafiador."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "b) Sinto-me cauteloso e preciso de um tempo para pensar antes de agir.",
+                afirmacao: [
+                    "Você pode ser mais ponderado e analítico, preferindo avaliar cuidadosamente antes de tomar decisões. Isso pode indicar que você é mais introvertido e prefere um ambiente mais controlado e previsível. "
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Como você prefere passar seu tempo livre?",
+        alternativas: [
+            {
+                texto: "a) Interagindo com amigos e participando de atividades sociais.",
+                afirmacao: [
+                    " Você pode ser uma pessoa extrovertida que se energiza ao estar em meio a outras pessoas e eventos sociais. A interação social é uma parte importante do seu bem-estar."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " b) Sozinho ou com um pequeno grupo de pessoas em um ambiente tranquilo.",
+                afirmacao: [
+                    " Você pode ser uma pessoa introvertida que valoriza momentos de solitude ou pequenas interações. Para você, é importante ter tempo para recarregar suas energias em um ambiente calmo."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "3. Como você lida com a organização e planejamento?",
+        alternativas: [
+            {
+                texto: "a) Gosto de manter tudo bem estruturado e seguir um plano detalhado.",
+                afirmacao: [
+                    "Você pode ser uma pessoa que valoriza a organização e a previsibilidade, gostando de ter um plano claro para alcançar seus objetivos. Isso pode indicar que você tem um estilo de vida mais estruturado."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "b) Prefiro ser flexível e lidar com as coisas conforme elas surgem.",
+                afirmacao: [
+                    "Você pode ser mais adaptável e aberto a mudanças, preferindo lidar com as situações conforme elas surgem. Isso pode indicar que você é mais espontâneo e confortável com a imprevisibilidade."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "4. Como você toma decisões importantes?",
+        alternativas: [
+            {
+                texto: "a) Baseio-me em fatos e dados objetivos para tomar uma decisão lógica.",
+                afirmacao: [
+                    " Você pode ser uma pessoa que valoriza a lógica e a análise crítica, preferindo tomar decisões com base em informações concretas. Isso pode indicar que você é mais racional e analítico."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "b) Confio mais nos meus sentimentos e intuições para decidir o que é melhor.",
+                afirmacao: [
+                    " Você pode ser alguém que valoriza a intuição e os sentimentos pessoais ao tomar decisões, confiando mais no que o seu coração diz. Isso pode indicar que você é mais emocional e subjetivo.",
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "5. Quando você está em um grupo, qual é o seu papel mais comum?",
+        alternativas: [
+            {
+                texto: "a) Eu tomo a iniciativa e conduzo as atividades.",
+                afirmacao: [
+                    "Você pode ser uma pessoa que gosta de liderar e tomar a frente em situações de grupo, mostrando habilidades de liderança e uma tendência a assumir responsabilidades."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "b) Eu observo e contribuo quando necessário, preferindo não ser o centro das atenções.",
+                afirmacao: [
+                    " Você pode ser mais reservado e preferir apoiar e contribuir nos bastidores, o que pode indicar que você é um bom ouvinte e colaborador."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "6. Quando você está em uma situação de conflito, como você tende a reagir?",
+        alternativas: [
+            {
+                texto: "a) Procuro resolver o conflito imediatamente, abordando a situação de frente.",
+                afirmacao: [
+                    "Você pode ser uma pessoa direta e assertiva, que acredita na importância de enfrentar os problemas de forma clara e objetiva. Isso pode indicar que você valoriza a resolução rápida e eficaz dos conflitos."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "b) Prefiro evitar o confronto e procurar uma solução pacífica de forma indireta.",
+                afirmacao: [
+                    "Você pode ser alguém que evita confrontos e prefere métodos mais sutis e diplomáticos para resolver desentendimentos. Isso pode indicar que você valoriza a harmonia e a paz nas relações."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "7. Quando você está aprendendo algo novo, qual abordagem você prefere?",
+        alternativas: [
+            {
+                texto: "7. Quando você está aprendendo algo novo, qual abordagem você prefere?",
+                afirmacao: [
+                    "Você pode ser uma pessoa que gosta de se dedicar intensamente e de forma rápida a novos desafios, mostrando entusiasmo e energia ao enfrentar novos aprendizados."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "b) Adoto uma abordagem mais gradual, aprendendo passo a passo e refletindo sobre cada etapa.",
+                afirmacao: [
+                    "Você pode preferir uma abordagem mais metódica e cuidadosa, dando-se tempo para absorver e refletir sobre as informações à medida que avança."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "8. Como você lida com mudanças inesperadas em sua rotina?",
+        alternativas: [
+            {
+                texto: "a) Adapto-me rapidamente e vejo as mudanças como oportunidades para crescimento",
+                afirmacao: [
+                    "Você pode ser uma pessoa que abraça a mudança e vê novas situações como chances para crescimento e inovação. Isso pode indicar flexibilidade e uma atitude positiva em relação ao inesperado."
+                ],
+            },
+            {
+                texto: "b) Sinto-me desconfortável e prefiro restaurar a rotina anterior o mais rápido possível.",
+                afirmacao: [
+                    "Você pode preferir estabilidade e rotinas bem estabelecidas, sentindo-se mais confortável quando as coisas seguem conforme o planejado. Isso pode indicar que você valoriza a previsibilidade e a segurança."
+                ],
+            },
+        ]
+    }
+];
